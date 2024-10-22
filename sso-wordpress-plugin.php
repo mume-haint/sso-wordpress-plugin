af86615b6ce653b9ff351b330c3bbdad79ccd201
haint <haint@mumesoft.vn>
2024-10-22 16:45:23 +0700

    uninstall plugin handler

--- sso-wordpress-plugin.php
+++ sso-wordpress-plugin.php
@@ -6,6 +6,10 @@ Version: 1.2
 Author: haint (Updated by Assistant)
 */
 
+if (!defined('ABSPATH')) {
+  exit;
+}
+
 require __DIR__ . '/vendor/autoload.php';
 
 require_once plugin_dir_path(__FILE__) . 'includes/class-keycloak-auth.php';
@@ -15,4 +19,18 @@ require_once plugin_dir_path(__FILE__) . 'includes/class-keycloak-sso-integratio
 
 if (class_exists('KeycloakSSOIntegration')) {
   new KeycloakSSOIntegration();
+}
+
+register_uninstall_hook(__FILE__, 'keycloak_sso_uninstall');
+
+/**
+ * Function to run on plugin uninstall.
+ * Deletes plugin-related options from the database.
+ */
+function keycloak_sso_uninstall() {
+  delete_option('keycloak_client_id');
+  delete_option('keycloak_client_secret');
+  delete_option('keycloak_url');
+  delete_option('keycloak_realm');
+  delete_option('keycloak_login_redirect_path');
 }
