57ed364e262ea0e672806b649ef14d76da45c97a
haint <haint@mumesoft.vn>
2024-10-29 15:25:04 +0700

    update test connection feature

--- includes/class-keycloak-settings.php
+++ includes/class-keycloak-settings.php
@@ -6,9 +6,11 @@ class KeycloakSettings
   {
     add_action('admin_menu', array($this, 'add_admin_menu'));
     add_action('admin_init', array($this, 'register_settings'));
+    add_action('wp_ajax_test_keycloak_connection', array($this, 'test_keycloak_connection'));
   }
 
-  public function add_admin_menu() {
+  public function add_admin_menu()
+  {
     add_options_page(
       'Keycloak SSO Settings',
       'Keycloak SSO',
@@ -18,16 +20,17 @@ class KeycloakSettings
     );
   }
 
-  public function register_settings() {
+  public function register_settings()
+  {
     // Register settings
     register_setting('keycloak_sso_settings_group', 'keycloak_client_id');
-//    register_setting('keycloak_sso_settings_group', 'keycloak_client_secret');
     register_setting('keycloak_sso_settings_group', 'keycloak_url');
     register_setting('keycloak_sso_settings_group', 'keycloak_realm');
     register_setting('keycloak_sso_settings_group', 'keycloak_login_redirect_path');
   }
 
-  public function settings_page() {
+  public function settings_page()
+  {
     ?>
     <div class="wrap">
       <h1>Keycloak SSO Settings</h1>
@@ -71,23 +74,77 @@ class KeycloakSettings
             <td><input type="text" name="keycloak_login_redirect_path" value="<?php echo esc_attr(get_option('keycloak_login_redirect_path')); ?>" style="width: 400px;" placeholder="/" /></td>
           </tr>
         </table>
+        <div style="display: flex; align-items: center; margin-top: 10px">
+          <button type="button" class="button-secondary" onclick="testKeycloakConnection()">Test Connection</button>
+          <span id="test-connection-result" style="margin-left: 10px; font-weight: bold;"></span>
+        </div>
         <?php submit_button(); ?>
       </form>
     </div>
+    <!--    <script type="text/javascript">-->
+    <!--        function toggleClientSecretVisibility() {-->
+    <!--            var input = document.getElementById('keycloak_client_secret');-->
+    <!--            var toggleIcon = document.getElementById('toggleClientSecret');-->
+    <!--            if (input.type === 'password') {-->
+    <!--                input.type = 'text';-->
+    <!--                toggleIcon.innerText = '🙈';-->
+    <!--            } else {-->
+    <!--                input.type = 'password';-->
+    <!--                toggleIcon.innerText = '👁️';-->
+    <!--            }-->
+    <!--        }-->
+    <!--    </script>-->
+    <script type="text/javascript">
+        function testKeycloakConnection() {
+            var resultElement = document.getElementById('test-connection-result');
+            resultElement.innerText = 'Testing...';
+            resultElement.style.color = '';
 
-<!--    <script type="text/javascript">-->
-<!--        function toggleClientSecretVisibility() {-->
-<!--            var input = document.getElementById('keycloak_client_secret');-->
-<!--            var toggleIcon = document.getElementById('toggleClientSecret');-->
-<!--            if (input.type === 'password') {-->
-<!--                input.type = 'text';-->
-<!--                toggleIcon.innerText = '🙈';-->
-<!--            } else {-->
-<!--                input.type = 'password';-->
-<!--                toggleIcon.innerText = '👁️';-->
-<!--            }-->
-<!--        }-->
-<!--    </script>-->
+            var data = {
+                action: 'test_keycloak_connection',
+                client_id: document.querySelector('input[name="keycloak_client_id"]').value,
+                keycloak_url: document.querySelector('input[name="keycloak_url"]').value,
+                realm: document.querySelector('input[name="keycloak_realm"]').value,
+            };
+
+            jQuery.post(ajaxurl, data, function(response) {
+                if (response.success) {
+                    resultElement.innerText = 'Connection successful!';
+                    resultElement.style.color = 'green';
+                } else {
+                    resultElement.innerText = 'Connection failed: ' + response.data;
+                    resultElement.style.color = 'red';
+                }
+            });
+        }
+    </script>
     <?php
+  }
+
+  public function test_keycloak_connection()
+  {
+    $client_id = sanitize_text_field($_POST['client_id']);
+    $url = rtrim(sanitize_text_field($_POST['keycloak_url']), '/');
+    $realm = sanitize_text_field($_POST['realm']);
+
+    // Construct the Keycloak URL for testing
+    $test_url = "{$url}/realms/{$realm}/protocol/openid-connect/auth";
+    $test_url .= '?response_type=code';
+    $test_url .= '&client_id=' . urlencode($client_id);
+    $test_url .= '&redirect_uri=' . urlencode(site_url());
+    $test_url .= '&scope=' . urlencode('openid profile email');
+    // Send a GET request to Keycloak
+    $response = wp_remote_get($test_url);
+    if (is_wp_error($response)) {
+      error_log("Error when testing config to Keycloak", $response->get_error_message());
+      wp_send_json_error($response->get_error_message());
+    } else {
+      $status_code = wp_remote_retrieve_response_code($response);
+      if ($status_code == 200) {
+        wp_send_json_success();
+      } else {
+        wp_send_json_error("Received status code: $status_code");
+      }
+    }
   }
 }
