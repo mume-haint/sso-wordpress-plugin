9ee2c2b5b1b792c75cce23dd6120add104e1bc19
haint <haint@mumesoft.vn>
2024-10-31 18:57:11 +0700

    update

--- sso-wordpress-plugin.php
+++ sso-wordpress-plugin.php
@@ -50,5 +50,13 @@ function sso_js_init_key()
 add_action('wp_enqueue_scripts','sso_js_init');
 
 function sso_js_init() {
-  wp_enqueue_script_module( 'keycloak-wp-js', plugins_url( '/js/keycloak-wp.js', __FILE__ ));
+  wp_register_script( 'keycloak-wp-js', plugins_url( '/js/keycloak-wp.js', __FILE__ ));
+
+  add_filter('script_loader_tag', function($tag, $handle, $src) {
+    if ('keycloak-wp-js' === $handle) {
+      $tag = '<script type="module" src="/' . esc_url($src) . '"></script>';
+    }
+    return $tag;
+  }, 10, 3);
+  wp_enqueue_script('keycloak-wp-js');
 }
